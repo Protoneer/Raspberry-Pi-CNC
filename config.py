0a5a5c8c0a4d6868f78ed5c42eb83ec33e6c1de6
Bertus Kruger <krugercreations@gmail.com>
2015-06-17 17:25:48 +1200

    Change the Title to indicate Beta version Change the default port to /dev/ttyAMA0

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 # Default Settings for GRBL0.9 and RPI
-serial_port = '/dev/ttyUSB0'
+
+serial_port = '/dev/ttyAMA0'
+#serial_port = '/dev/ttyUSB0'
+
 serial_baud = 9600
+#serial_baud = 115200
+
 serial_timeout = 0.1
